29f7035ca31c3a8c8cac3ece2a9c9e869e527eee
Kummirai <ajaxmilton@hotmail.com>
2022-09-07 15:59:39 +0200

    Main program

---
+++ monty.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
+/**
+ * main - main function
+ * Return: 0 on success
+ *
+ */
+int main(void)
+{
+	push(1);
+	push(2);
+	push(3);
+	pall();
+	return (0);
+}
+
